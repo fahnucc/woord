f9f9d1f8c3151e96f91c0c5752ba84d88f1acfc1
fahnu <eskicifahri@gmail.com>
2024-10-26 15:43:36 +0300

    implement header

---
+++ packages/client/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Logo from "./Logo";
+
+const Header = () => {
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  const navList = [
+    { name: "Home", to: "/" },
+    { name: "Rooms", to: "/rooms" },
+    { name: "About", to: "/about" },
+  ];
+
+  return (
+    <header className="bg-green-300 w-full h-16 px-4 flex items-center justify-between">
+      <div className="flex items-center h-full gap-8">
+        <Logo
+          className="h-2/3 w-max text-transparent"
+          onClick={() => navigate("/")}
+        />
+        <nav className="flex h-full justify-center items-center">
+          {navList.map((navItem) => (
+            <div className="hover:underline hover:bg-green-500 h-full flex items-center px-3">
+              <Link to={navItem.to}>{navItem.name}</Link>
+            </div>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- packages/client/src/layout/Layout.js
+++ packages/client/src/layout/Layout.js
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col h-screen w-full bg-purple-300">
+      <Header />
       <div className="flex flex-1">
         <div className="flex-1 flex items-center justify-center">
           <div className="bg-blue-300 p-4 w-full max-w-4xl h-full max-h-[calc(100vh-5rem)] overflow-auto rounded-3xl">
